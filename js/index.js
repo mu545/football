aea7b4bf6b0904228b04d921483bcca32dd643de
musa sutisna <mus4.sutisn4@gmail.com>
2019-11-15 22:39:54 +0700

    Response

--- js/index.js
+++ js/index.js
@@ -13,52 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
   loadPage(currentPage)
 
   /**
-   * Handle fetch response status.
-   *
-   * @param   object
-   * @return  promise
-   */
-  function responseStatus(fetchResponse) {
-    if (fetchResponse.status !== 200) {
-      return Promise.reject(new Error(fetchResponse.statusText))
-    } else {
-      return Promise.resolve(fetchResponse)
-    }
-  }
-
-  /**
-   * Handle fetch response
-   * parsing to plain text.
-   *
-   * @param   object
-   * @return  promise
-   */
-  function responseText(fetchResponse) {
-    return fetchResponse.text()
-  }
-
-  /**
-   * Handle fetch response
-   * parsing to json.
-   *
-   * @param   object
-   * @return  promise
-   */
-  function responseJSON(fetchResponse) {
-    return fetchResponse.json()
-  }
-
-  /**
-   * Console log error message.
-   *
-   * @param   mixed
-   * @return  void
-   */
-  function logError(error) {
-    console.log(`Error: ${error}`)
-  }
-
-  /**
    * Load available navigation
    * fill loaded navigation to nav.
    *
--- main.js
+++ main.js
@@ -12,3 +12,49 @@ if ('serviceWorker' in navigator) {
 } else {
   console.log('Service worker: browser not support')
 }
+
+/**
+ * Handle fetch response status.
+ *
+ * @param   object
+ * @return  promise
+ */
+function responseStatus(fetchResponse) {
+  if (fetchResponse.status !== 200) {
+    return Promise.reject(new Error(fetchResponse.statusText))
+  } else {
+    return Promise.resolve(fetchResponse)
+  }
+}
+
+/**
+ * Handle fetch response
+ * parsing to plain text.
+ *
+ * @param   object
+ * @return  promise
+ */
+function responseText(fetchResponse) {
+  return fetchResponse.text()
+}
+
+/**
+ * Handle fetch response
+ * parsing to json.
+ *
+ * @param   object
+ * @return  promise
+ */
+function responseJSON(fetchResponse) {
+  return fetchResponse.json()
+}
+
+/**
+ * Console log error message.
+ *
+ * @param   mixed
+ * @return  void
+ */
+function logError(error) {
+  console.log(`Error: ${error}`)
+}
